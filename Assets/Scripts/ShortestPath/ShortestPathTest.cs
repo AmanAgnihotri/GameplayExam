595c3096fc6ff37751d894afd68e1c00f410810f
Aman Agnihotri <awfully.awesome@yahoo.com>
2015-03-30 17:17:57 +0530

    Add ShortestPathTest Partial Implementation
    This implementation creates a graph to be used for finding shortest
    paths between its vertices.
    
    The shortest path finding method calls will be added later.

---
+++ Assets/Scripts/ShortestPath/ShortestPathTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Exam.ShortestPath
+{
+  public class ShortestPathTest : MonoBehaviour
+  {
+    public void Start ()
+    {
+      IVertex vertexA = new Vertex ("A", new Vector2 (0, 0));
+      IVertex vertexB = new Vertex ("B", new Vector2 (1, 2));
+      IVertex vertexC = new Vertex ("C", new Vector2 (3, 4));
+      IVertex vertexD = new Vertex ("D", new Vector2 (2, 1));
+      IVertex vertexE = new Vertex ("E", new Vector2 (4, 1));
+      IVertex vertexF = new Vertex ("F", new Vector2 (5, 3));
+
+      IGraphBuilder graphBuilder = new GraphBuilder ();
+
+      IGraph graph = graphBuilder
+        .AddVertex (vertexA).AddVertex (vertexB)
+        .AddVertex (vertexC).AddVertex (vertexD)
+        .AddVertex (vertexE).AddVertex (vertexF)
+        .AddEdge (vertexA, vertexB)
+        .AddEdge (vertexB, vertexC)
+        .AddEdge (vertexB, vertexD)
+        .AddEdge (vertexC, vertexE)
+        .AddEdge (vertexD, vertexE)
+        .AddEdge (vertexD, vertexF)
+        .Build ();
+    }
+  }
+}
